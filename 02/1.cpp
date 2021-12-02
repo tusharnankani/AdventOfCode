934f8d75ddc4609db7d47189959462085c37dd3e
tusharnankani <tusharnankani3@gmail.com>
2021-12-03 00:30:01 +0530

    :sparkles: feat: add Day 2 (Part 1 + 2)

---
+++ 02/1.cpp
@@ -0,0 +1,65 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int           ll;
+typedef unsigned long long int  ull;
+typedef long double             ld;
+
+typedef pair<ll,ll>             pll;
+typedef vector<ll>              vll;
+typedef vector<pll>             vpll;
+typedef map<ll, ll>             mll;
+typedef map<char, ll>           mcl;
+
+#define inf                 1<<30
+#define mx9                 1000000007
+#define test(t)             int t; cin >> t; while(t--)
+#define f(i, a, b)          for(int (i) = (a); (i) < (b); ++(i))
+#define all(a)              (a).begin(), (a).end()
+#define endl                "\n"
+#define ff                  first
+#define ss                  second
+#define pb                  push_back
+#define deb(x)              cout << #x << ": " << x << endl;
+#define deb2(x,y)           cout << #x << ": " << x << " ~ " <<  #y << ": " << y << endl;
+#define deba(arr)           cout << #arr << " ~ [ "; for(auto n: arr) cout << n << " "; cout << "]" << endl;
+#define debap(arr)          cout << #arr << " ~ [ "; for(auto n: arr) cout << n.first << "-" << n.second << " "; cout << "]" << endl;
+#define fast                ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve()
+{
+    string s;
+    vll a;
+    ll cnt = 0;
+    while (cin >> s) {
+        string curr = s;
+        ll n;
+        cin >> n;
+        if(curr == "forward") {
+            cnt += n;
+        }
+        else if(curr == "up") {
+            a.pb(n);
+        }
+        else if(curr == "down") {
+            a.pb(-n);
+        }
+    }
+    cout << abs(accumulate(all(a), 0)) * cnt << endl;
+}
+
+int main()
+{
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    // test(t)
+        solve();
+    return 0;
+}
