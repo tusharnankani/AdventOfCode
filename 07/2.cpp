fb2f0b12e4f35c4f159206641ee1fd788d1a87ef
tusharnankani <tusharnankani3@gmail.com>
2021-12-29 03:55:35 +0530

    :sparkles: feat: add Day 7 (Part 1 + 2)

---
+++ 07/2.cpp
@@ -0,0 +1,93 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int           ll;
+typedef unsigned long long int  ull;
+typedef long double             ld;
+
+typedef pair<ll,ll>             pll;
+typedef vector<ll>              vll;
+typedef vector<pll>             vpll;
+typedef map<ll, ll>             mll;
+typedef map<char, ll>           mcl;
+
+#define inf                 1<<30
+#define mx9                 1000000007
+#define test(t)             int t; cin >> t; while(t--)
+#define f(i, a, b)          for(int (i) = (a); (i) < (b); ++(i))
+#define all(a)              (a).begin(), (a).end()
+#define endl                "\n"
+#define ff                  first
+#define ss                  second
+#define pb                  push_back
+#define deb(x)              cout << #x << ": " << x << endl;
+#define deb2(x,y)           cout << #x << ": " << x << " ~ " <<  #y << ": " << y << endl;
+#define deba(arr)           cout << #arr << " ~ [ "; for(auto n: arr) cout << n << " "; cout << "]" << endl;
+#define debap(arr)          cout << #arr << " ~ [ "; for(auto n: arr) cout << n.first << "-" << n.second << " "; cout << "]" << endl;
+#define fast                ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+ll go(ll n) {
+    ll sum = 0;
+    if(n % 2) {
+        sum = n * ((n + 1) / 2);
+    }
+    else {
+        sum = (n / 2) * (n + 1);
+    }
+    return sum;
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+    vll order;
+    ll curr = 0;
+    f(i, 0, s.length()) {
+        if(s[i] == ',') {
+            order.pb(curr);
+            curr = 0;
+        } else {
+            curr = curr * 10 + (s[i] - '0');
+        }
+    }
+    order.pb(curr);
+    // deba(order)
+    
+    map<ll, ll> mp;
+    set<ll> st;
+    ll mn = inf, mx = 0;
+    for(auto it: order) {
+        mp[it] += 1;
+        st.insert(it);
+        mn = min(mn, it);
+        mx = max(mx, it);
+    }
+    // deba(st)
+    // debap(mp)
+
+    ll ans = LONG_MAX;
+    f(i, mn, mx + 1) {
+        ll curr = 0;
+        for(auto x: mp) {
+            curr += (x.ss * go(abs(x.ff - i)));
+        }
+        ans = min(ans, curr);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+        solve();
+    return 0;
+}
